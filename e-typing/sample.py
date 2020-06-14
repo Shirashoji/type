8df4971f35179e684d63be2b26d83aa550e32b69
Shirashoji <tkm.shira@gmail.com>
2020-06-14 12:03:35 +0900

    commit

--- e-typing/sample.py
+++ e-typing/sample.py
@@ -1,10 +1,11 @@
 import type, time
 
 
-type.browser_setting('firefox')
-type.create_account()
-wait_time = 0.09
-type.typing_first(WAIT_SECOND = wait_time, ranking='on')
+type.browser_setting('tor')
+type.create_account_with_mail(Mail_address="keroro-gunso@fanclub.pm",User_Name="ケロロ軍曹@ぁ〜ん")
+#type.login('めあど',"pass")
+wait_time = 0.08
+#type.typing_first(WAIT_SECOND = wait_time, ranking='on')
 for _ in range(80):
     wait_time -= 0.0005
     type.typing(WAIT_SECOND = wait_time, ranking='on')
--- e-typing/type.py
+++ e-typing/type.py
@@ -14,6 +14,7 @@ def browser_setting(Browser):
     if Browser.lower()=='chrome':
         import chromedriver_binary
         chrome_options = webdriver.ChromeOptions()
+        chrome_options.add_argument("--incognito")
         chrome_options.add_experimental_option("excludeSwitches", ['enable-automation'])
         driver = webdriver.Chrome(options=chrome_options)
     elif Browser.lower()=='tor':
@@ -32,11 +33,11 @@ def browser_setting(Browser):
 def login(Mail_address, Mail_password):
     driver.get('https://www.e-typing.ne.jp/')
     driver.maximize_window()
-    WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.ID, 'mail')))
+    WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.ID, 'mail')))
     driver.find_element_by_id("mail").send_keys(Mail_address)
-    WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.ID, 'password')))
+    WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.ID, 'password')))
     driver.find_element_by_id("password").send_keys(Mail_password)
-    WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.ID, 'login_btn')))
+    WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.ID, 'login_btn')))
     driver.find_element_by_id("login_btn").click()
 
 def create_account():
@@ -44,27 +45,50 @@ def create_account():
     driver.get('https://www.e-typing.ne.jp/signup/signup.asp')
     driver.maximize_window()
     global Mail_address, Mail_password
-    Mail_address = ''.join([random.choice(string.ascii_letters + string.digits) for i in range(4)])+"_@gmail.com" #自動生成
-    Mail_password = ''.join([random.choice(string.ascii_letters + string.digits) for i in range(15)]) #自動生成
-    WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.ID, 'name')))
-    driver.find_element_by_id("name").send_keys("aaa")
-    WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.ID, 'email')))
+    WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.ID, 'name')))
+    driver.find_element_by_id("name").send_keys("tutsu☆jiji")
+    WebDriverWait(driver, 15).until(EC.presence_of_element_located((By.ID, 'email')))
     driver.find_element_by_id("email").send_keys(Mail_address)
-    WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.ID, 'email_re')))
+    WebDriverWait(driver, 15).until(EC.presence_of_element_located((By.ID, 'email_re')))
     driver.find_element_by_id("email_re").send_keys(Mail_address)
-    WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.ID, 'su_password')))
+    WebDriverWait(driver, 15).until(EC.presence_of_element_located((By.ID, 'su_password')))
     driver.find_element_by_id("su_password").send_keys(Mail_password)
-    WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.ID, 'target')))
-    driver.find_element_by_id("target").send_keys("aaa")
+    WebDriverWait(driver, 15).until(EC.presence_of_element_located((By.ID, 'target')))
+    driver.find_element_by_id("target").send_keys("tutsu☆jiji")
     driver.execute_script("document.getElementById('regist_btn').scrollIntoView()")
     time.sleep(3)
-    WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.ID, 'regist_btn')))
+    WebDriverWait(driver, 15).until(EC.presence_of_element_located((By.ID, 'regist_btn')))
     driver.find_element_by_xpath("//button[@id='regist_btn']").click()
     time.sleep(3)
-    WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.ID, 'mh_btn')))
+    WebDriverWait(driver, 15).until(EC.presence_of_element_located((By.ID, 'mh_btn')))
     driver.find_element_by_xpath("//img[@alt='会員ホームへ']").click()
     print('ログインのメールアドレス: '+Mail_address)
     print('ログインのパスワード: '+Mail_password)
+
+def create_account_with_mail(Mail_address, Mail_password="pass",User_Name="NAMELESS"):
+    import random, string
+    driver.get('https://www.e-typing.ne.jp/signup/signup.asp')
+    driver.maximize_window()
+    WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.ID, 'name')))
+    driver.find_element_by_id("name").send_keys(User_Name)
+    WebDriverWait(driver, 15).until(EC.presence_of_element_located((By.ID, 'email')))
+    driver.find_element_by_id("email").send_keys(Mail_address)
+    WebDriverWait(driver, 15).until(EC.presence_of_element_located((By.ID, 'email_re')))
+    driver.find_element_by_id("email_re").send_keys(Mail_address)
+    WebDriverWait(driver, 15).until(EC.presence_of_element_located((By.ID, 'su_password')))
+    driver.find_element_by_id("su_password").send_keys(Mail_password)
+    WebDriverWait(driver, 15).until(EC.presence_of_element_located((By.ID, 'target')))
+    driver.find_element_by_id("target").send_keys("tutsu☆jiji")
+    driver.execute_script("document.getElementById('regist_btn').scrollIntoView()")
+    time.sleep(3)
+    WebDriverWait(driver, 15).until(EC.presence_of_element_located((By.ID, 'regist_btn')))
+    driver.find_element_by_xpath("//button[@id='regist_btn']").click()
+    time.sleep(3)
+    WebDriverWait(driver, 15).until(EC.presence_of_element_located((By.ID, 'mh_btn')))
+    driver.find_element_by_xpath("//img[@alt='会員ホームへ']").click()
+    print('ログインのメールアドレス: '+Mail_address)
+    print('ログインのパスワード: '+Mail_password)
+
 
 def typing(WAIT_SECOND = 0.03, ranking='off'):
     driver.get('https://www.e-typing.ne.jp/')
@@ -94,13 +118,13 @@ def typing(WAIT_SECOND = 0.03, ranking='off'):
 
 def typing_first(WAIT_SECOND = 0.03, ranking='off'):
     driver.get('https://www.e-typing.ne.jp/')
-    WebDriverWait(driver, 15).until(EC.presence_of_element_located((By.XPATH, '//*[@id="level_check_member"]/a')))
+    WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.XPATH, '//*[@id="level_check_member"]/a')))
     driver.find_element_by_xpath('//*[@id="level_check_member"]/a').click()
-    WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.ID, 'typing_content')))
+    WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.ID, 'typing_content')))
     driver.switch_to_frame('typing_content')
-    WebDriverWait(driver, 15).until(EC.presence_of_element_located((By.ID, 'start_btn')))
+    WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.ID, 'start_btn')))
     driver.find_element_by_id("start_btn").click()
-    WebDriverWait(driver, 15).until(EC.presence_of_element_located((By.ID, 'start_msg')))
+    WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.ID, 'start_msg')))
     driver.find_element_by_tag_name("body").send_keys(Keys.SPACE)
     time.sleep(3)
     for i in range(0, 12):
@@ -109,10 +133,10 @@ def typing_first(WAIT_SECOND = 0.03, ranking='off'):
             driver.find_element_by_tag_name("body").send_keys(key)
             time.sleep(WAIT_SECOND)
     if ranking.lower() == 'on':
-        WebDriverWait(driver, 15).until(EC.presence_of_element_located((By.ID, 'regist_btn')))
+        WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.ID, 'regist_btn')))
         driver.find_element_by_id("regist_btn").click()
         try:
-            WebDriverWait(driver, 15).until(EC.presence_of_element_located((By.ID, 'yesBtn')))
+            WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.ID, 'yesBtn')))
             driver.find_element_by_id("yesBtn").click()
         except:
             print('点数が高すぎるため登録不可能')
